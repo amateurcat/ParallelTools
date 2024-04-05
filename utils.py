36ace5adc6f71369a77e17d29964c32661d3b23d
amateurcat <thisisapostbox@gmail.com>
2024-04-05 01:17:31 -0400

    04052024@Braavos: add TempDir and in_tempdir to utils.py, they are commonly used in our tasks

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import io
+import io, os, tempfile, shutil
 
 def read_chunks(read_from, chunk_size):
     with open(read_from, 'r') as file:
@@ -8,3 +8,58 @@ def read_chunks(read_from, chunk_size):
                 break
             string_io = io.StringIO(chunk)
             yield string_io
+
+class TempDir(object):
+    """
+    class for temporary directories
+    creates a (named) directory which is deleted after use.
+    All files created within the directory are destroyed
+    Might not work on windows when the files are still opened
+    """
+
+    def __init__(self, suffix="", prefix="tmp", basedir=None):
+        self.name = tempfile.mkdtemp(suffix=suffix, prefix=prefix, dir=basedir)
+
+    def __del__(self):
+        try:
+            if self.name:
+                self.dissolve()
+        except AttributeError:
+            pass
+
+    def __enter__(self):
+        return self.name
+
+    def __exit__(self, *errstuff):
+        self.dissolve()
+        #pass
+
+    def dissolve(self):
+        """remove all files and directories created within the tempdir"""
+        if self.name:
+            shutil.rmtree(self.name)
+        self.name = ""
+
+    def __str__(self):
+        if self.name:
+            return "temporary directory at: %s" % (self.name,)
+        else:
+            return "dissolved temporary directory"
+
+
+class in_tempdir(object):
+    """Create a temporary directory and change to it.  """
+
+    def __init__(self, delete_temp=True, *args, **kwargs):
+        self.delete_temp = delete_temp
+        self.tmpdir = TempDir(*args, **kwargs)
+
+    def __enter__(self):
+        self.old_path = os.getcwd()
+        os.chdir(self.tmpdir.name)
+        return self.tmpdir.name
+
+    def __exit__(self, *errstuff):
+        os.chdir(self.old_path)
+        if self.delete_temp:
+            self.tmpdir.dissolve()
